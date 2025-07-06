75e57f656ffb7998659d08a9c573600d86107e16
Gustavo Rodrigues <gustavo.rodrigues@sgrentals.com.br>
2025-07-05 21:31:52 -0300

    commit teste pipe

--- src/FastTechFoods.Application/Services/ProductService.cs
+++ src/FastTechFoods.Application/Services/ProductService.cs
@@ -9,6 +9,7 @@ public class ProductService : IProductService
 {
     private readonly IProductRepository _repository;
 
+
     public ProductService(IProductRepository repository)
     {
         _repository = repository;
